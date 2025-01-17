efcb55a0a7ec4a81d017e5c81e1ee9d9c612eea5
Jamie Thomas <jamiewthomas13@gmail.com>
2025-01-17 08:53:56 +0000

    Handle multiple allocations and double frees

--- alligator.zig
+++ alligator.zig
@@ -1,30 +1,62 @@
 const std = @import("std");
 const Error = std.mem.Allocator.Error;
 
+const AlligationMeta = struct {
+    address: *u8,
+    size: usize,
+};
+
 pub const Alligator = struct {
     base: *const std.mem.Allocator,
-    totalAlligated: usize,
-    totalFreed: usize,
+    total_alligated: usize,
+    total_freed: usize,
+    alligations: std.AutoHashMap(*u8, AlligationMeta),
 
     pub fn init(base: *const std.mem.Allocator) Alligator {
         return Alligator{
             .base = base,
-            .totalAlligated = 0,
-            .totalFreed = 0,
+            .total_alligated = 0,
+            .total_freed = 0,
+            .alligations = std.AutoHashMap(*u8, AlligationMeta).init(base.*),
         };
     }
 
-    pub fn alligate(self: *Alligator, comptime T: type, count: usize) Error![]T {
-        const ptr = try self.base.alloc(T, count);
-
-        self.totalAlligated += ptr.len;
-
-        return ptr;
+    pub fn deinit(self: *Alligator) void {
+        self.alligations.deinit();
     }
 
-    pub fn free(self: *Alligator, buffer: anytype) void {
-        const totalToFree = buffer.len;
+    pub fn alligate(self: *Alligator, comptime T: type, count: usize) Error![]T {
+        const buffer: []T = try self.base.alloc(T, count);
+
+        const address = &buffer[0];
+        try self.alligations.put(address, AlligationMeta{
+            .address = address,
+            .size = count * @sizeOf(T),
+        });
+
+        self.total_alligated += buffer.len * @sizeOf(T);
+
+        std.debug.print("Added pointer {} with count {} to alligations map\n", .{ address, count });
+
+        return buffer;
+    }
+
+    pub fn free(self: *Alligator, comptime T: type, buffer: []T) void {
+        const address = &buffer[0];
+        const total_to_free = buffer.len * @sizeOf(T);
+
+        std.debug.print("Attempting to free at address {}\n", .{address});
+
+        const alligation: AlligationMeta = self.alligations.get(address) orelse {
+            std.debug.print("No alligation found for pointer {}\n", .{address});
+            return;
+        };
+
+        std.debug.print("Found alligated buffer:\n{}\n", .{alligation});
+        std.debug.print("Freeing active buffer: {s}\n", .{address});
+
         self.base.free(buffer);
-        self.totalFreed += totalToFree;
+        self.total_freed += total_to_free;
+        _ = self.alligations.remove(address);
     }
 };
--- test_alligator.zig
+++ test_alligator.zig
@@ -10,11 +10,12 @@ test "alligate memory" {
     }
 
     var alligator = Alligator.init(&gpa.allocator());
+    defer alligator.deinit();
 
     const buffer = try alligator.alligate(u8, 128);
     try expect(@TypeOf(buffer) == []u8);
     try expect(buffer.len == 128);
-    try expect(alligator.totalAlligated == 128);
+    try expect(alligator.total_alligated == 128);
 }
 
 test "free memory" {
@@ -25,48 +26,50 @@ test "free memory" {
     }
 
     var alligator = Alligator.init(&gpa.allocator());
+    defer alligator.deinit();
 
     const buffer = try alligator.alligate(u8, 128);
-    alligator.free(buffer);
-    try expect(alligator.totalFreed == 128);
-    try expect(alligator.totalAlligated - alligator.totalFreed == 0);
+    alligator.free(u8, buffer);
+    try expect(alligator.total_freed == 128);
+    try expect(alligator.total_alligated - alligator.total_freed == 0);
 }
 
-// test "net usage tracking" {
-//     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-//     defer {
-//         const deinit_status = gpa.deinit();
-//         if (deinit_status == .leak) expect(false) catch @panic("TEST FAILED: GeneralPurposeAllocator leaked memory");
-//     }
-//
-//     var alligator = Alligator.init(&gpa.allocator());
-//
-//     const buffer1 = try alligator.alligate(u8, 128);
-//     const buffer2 = try alligator.alligate(u8, 64);
-//     alligator.free(buffer1, u8, 128);
-//     _ = buffer2;
-//
-//     try expect(alligator.totalAlligated == 192);
-//     try expect(alligator.totalFreed == 128);
-//     try expect(alligator.totalAlligated - alligator.totalFreed == 64);
-// }
-//
-// test "double free prevention" {
-//     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-//     defer {
-//         const deinit_status = gpa.deinit();
-//         if (deinit_status == .leak) expect(false) catch @panic("TEST FAILED: GeneralPurposeAllocator leaked memory");
-//     }
-//
-//     var alligator = Alligator.init(&gpa.allocator());
-//
-//     const buffer = try alligator.alligate(u8, 128);
-//     alligator.free(buffer);
-//     alligator.free(buffer); // Double free
-//
-//     try expect(alligator.totalFreed == 128);
-// }
-//
+test "net usage tracking" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) expect(false) catch @panic("TEST FAILED: GeneralPurposeAllocator leaked memory");
+    }
+
+    var alligator = Alligator.init(&gpa.allocator());
+    defer alligator.deinit();
+
+    const buffer1 = try alligator.alligate(u8, 128);
+    const buffer2 = try alligator.alligate(u8, 64);
+    alligator.free(u8, buffer1);
+
+    try expect(alligator.total_alligated == 192);
+    try expect(alligator.total_freed == 128);
+    alligator.free(u8, buffer2);
+}
+
+test "double free prevention" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) expect(false) catch @panic("TEST FAILED: GeneralPurposeAllocator leaked memory");
+    }
+
+    var alligator = Alligator.init(&gpa.allocator());
+    defer alligator.deinit();
+
+    const buffer = try alligator.alligate(u8, 128);
+    alligator.free(u8, buffer);
+    alligator.free(u8, buffer); // Double free
+
+    try expect(alligator.total_freed == 128);
+}
+
 // test "null free handling" {
 //     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 //     defer {
@@ -78,7 +81,7 @@ test "free memory" {
 //
 //     alligator.free(null, u8, 128); // Freeing null
 //
-//     try expect(alligator.totalFreed == 0);
+//     try expect(alligator.total_freed == 0);
 // }
 //
 // test "freeing wrong size" {
@@ -93,7 +96,7 @@ test "free memory" {
 //     const buffer = try alligator.alligate(u8, 128);
 //     alligator.free(buffer, u8, 64); // Freeing wrong size
 //
-//     try expect(alligator.totalFreed == 0);
+//     try expect(alligator.total_freed == 0);
 // }
 //
 // test "freeing wrong type" {
@@ -108,7 +111,7 @@ test "free memory" {
 //     const buffer = try alligator.alligate(u8, 128);
 //     alligator.free(buffer, u16, 128); // Freeing wrong type
 //
-//     try expect(alligator.totalFreed == 0);
+//     try expect(alligator.total_freed == 0);
 // }
 //
 // test "zero allocation handling" {
@@ -123,5 +126,5 @@ test "free memory" {
 //     const buffer = try alligator.alligate(u8, 0);
 //
 //     try expect(buffer == null);
-//     try expect(alligator.totalAlligated == 0);
+//     try expect(alligator.total_alligated == 0);
 // }
